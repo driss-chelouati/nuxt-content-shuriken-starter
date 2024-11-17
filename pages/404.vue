91ed3b3845ba53139a76f770089edcebf97dd642
Driss Chelouati <driss.chelouati@gmail.com>
2024-11-17 13:39:45 +0100

    feat: add 404 page redirection

---
+++ pages/404.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+definePageMeta({
+  title: '404 Page not found',
+  layout: 'empty',
+})
+</script>
+
+<template>
+  <div>
+    Page not found
+  </div>
+</template>
---
+++ pages/[route].vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const router = useRouter()
+const { path } = useRoute()
+
+function redirectIfPagePathIsInvalid() {
+  // Replace this "false" your own logic to verify if the path is valid
+  const isPathValid = (path: any) => false
+  if (!isPathValid(path)) {
+    router.push({ path: '/404' })
+  }
+}
+redirectIfPagePathIsInvalid()
+</script>
